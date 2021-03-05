ef66109e2d40322bed644082b4924d212c36dd4e
vPastor <victorpastorcaro@gmail.om>
2021-03-05 15:20:53 +0100

    commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var dotenv = require('dotenv');
-
+var io = require("socket.io").listen(server);
 var app = express();
 var path = require("path");
 var router = express.Router();
@@ -12,9 +12,15 @@ var server = require("http")
 .listen(port,()=>{
     console.log("server running on: "+port);
 });
+var io = require("socket.io").listen(server);
 app.use(express.static(__dirname+'/views'));
 router.get("/",function(req,res,next){
     res.render("index",{title:"Express"});
 });
+io.sockets.on('connection', function(socket){
+    socket.on('sendMessage',function(data){
+        io.socket.emit('newMessage',{msg:data});
+    });
+});  
 app.use("/",router);
 module.exports=app;
