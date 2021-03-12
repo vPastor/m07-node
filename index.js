df11a143dedc5fd7bbb46f6c767771c7249e4d05
vPastor <victorpastorcaro@gmail.om>
2021-03-12 14:27:52 +0100

    last

--- index.js
+++ index.js
@@ -4,23 +4,36 @@ var io = require("socket.io").listen(server);
 var app = express();
 var path = require("path");
 var router = express.Router();
-dotenv.config();
+
+//ENRUTADOR REDIRIGIENDO A ROUTES/GETS
+//SE PUEDE HACER CON CUALQUIER SITUACION
+var getRoutes = require('./routes/gets');
+const { get } = require("https");
+app.use('/', getRoutes);
+
+
+
+//INTENTANDO IMPORTAR LAS VARIABLES DE .ENV
 const port = process.env.PORT;
-console.log(`Your port is ${port}`);
+dotenv.config();
+console.log('Your port is '+port);
+
+//ESTA FUNCION NO RECUERDO PARA QUE SIRVE
 var server = require("http")
 .createServer(app)
 .listen(port,()=>{
     console.log("server running on: "+port);
 });
-var io = require("socket.io").listen(server);
+//ESTO ES PARA DECIRLE A EXPRESS DE DONDE COGER LAS VISTAS
 app.use(express.static(__dirname+'/views'));
-router.get("/",function(req,res,next){
-    res.render("index",{title:"Express"});
-});
+//SOCKET NI IDEA
+var io = require("socket.io").listen(server);
 io.sockets.on('connection', function(socket){
     socket.on('sendMessage',function(data){
         io.socket.emit('newMessage',{msg:data});
     });
 });  
+
+//ESTAS DOS LINEAS NO RECUERDO
 app.use("/",router);
 module.exports=app;
